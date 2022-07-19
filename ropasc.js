10956f5d1cf657adbb0391eff0db1a210d61ecce
Savannah Romberg <savannah.romberg@gmail.com>
2022-07-19 16:51:18 -0600

    adding second function with two parameters. step 4 & 5 on TOP

--- ropasc.js
+++ ropasc.js
@@ -3,6 +3,15 @@ const choices = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
     const random = Math.floor(Math.random() * choices.length);
     console.log(choices[random]);
+    return choices[random];
 }
 
-getComputerChoice();
+let computerSelection = getComputerChoice();
+let playerSelection = prompt('Rock, paper, or scissors?').toLowerCase();
+
+//function functionName(playerSelection, computerSelection) {
+    // code to be executed
+//  }
+
+console.log(playerSelection, computerSelection);
+  
