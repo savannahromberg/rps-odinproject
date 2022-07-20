28f16f8a4383ea368665f0aa71a710067dc12492
Savannah Romberg <savannah.romberg@gmail.com>
2022-07-20 16:37:47 -0600

    finished 9 iterations of if else if loop. working on displaying result to screen without 'document.write()', which is not common practice

--- ropasc.js
+++ ropasc.js
@@ -5,22 +5,35 @@ function getComputerChoice() {
     return choices[random];
 }
 
-let rockPaper = 'Paper beats Rock!';
-let scissorsRock = 'Rock beats Scissors!';
-let paperScissors = 'Scissors beats Paper!';
-let playerWin = 'You win!';
-let playerLose = 'You lose!';
+const rockPaper = "Paper beats Rock! ";
+const scissorsRock = "Rock beats Scissors! ";
+const paperScissors = "Scissors beats Paper! ";
+const playerWin = "You win!";
+const playerLose = "You lose!";
+const playerTie = "It's a tie!"
 
 let computerSelection = getComputerChoice();
-let playerSelection = prompt('Rock, paper, or scissors?').toUpperCase();
+let playerSelection = prompt("Rock, paper, or scissors?").toUpperCase();
 
 function gameRound(playerSelection, computerSelection) {
-    if (playerSelection === 'ROCK', computerSelection === 'SCISSORS') {
-        alert(scissorsRock, ' You win!');
-    } else if (playerSelection === 'PAPER', computerSelection === 'ROCK') {
-        alert(rockPaper);
-    } else if (playerSelection === 'SCISSORS', computerSelection === 'PAPER') {
-        alert(paperScissors);
+    if (playerSelection === "ROCK", computerSelection === "SCISSORS") {
+        document.write(scissorsRock, playerWin);
+    } else if (playerSelection === "ROCK", computerSelection === "PAPER") {
+        document.write(rockPaper, playerLose);
+    } else if (playerSelection === "ROCK", computerSelection === "ROCK") {
+       document.write(paperScissors, playerTie);
+    } else if (playerSelection === "SCISSORS", computerSelection === "PAPER") {
+        document.write(paperScissors, playerWin);
+    } else if (playerSelection === "SCISSORS", computerSelection === "ROCK") {
+            document.write(paperScissors, playerLose);
+    } else if (playerSelection === "SCISSORS", computerSelection === "SCISSORS") {
+            document.write(paperScissors, playerTie);
+    } else if (playerSelection === "PAPER", computerSelection === "ROCK") {
+            document.write(paperScissors, playerWin);
+    } else if (playerSelection === "PAPER", computerSelection === "SCISSORS") {
+            document.write(paperScissors, playerLose);
+    } else if (playerSelection === "PAPER", computerSelection === "PAPER") {
+            document.write(paperScissors, playerTie);
     }
 }
 
