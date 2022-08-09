4b30cc32ac86810d45b6bbeeec4ae6609e602dc1
Savannah Romberg <savannah.romberg@gmail.com>
2022-08-09 11:55:47 -0600

    buttons working, counters working, game-to-5 working

--- ropasc.js
+++ ropasc.js
@@ -1,23 +1,45 @@
+//Array of computer choices
 const choices = ["ROCK", "PAPER", "SCISSORS"];
 
-const rockPaper = "Paper covers Rock! <br>";
-const scissorsRock = "Rock smashes Scissors! <br>";
-const paperScissors = "Scissors cut Paper! <br>";
-const playerWin = "YOU WIN! <br>";
-const playerLose = "YOU LOSE! <br>";
-const playerTie = "It's a tie! <br>"
+//Variables explaining which ones beat the others and who won
+const rockPaper = "Paper covers Rock! \n";
+const scissorsRock = "Rock smashes Scissors! \n";
+const paperScissors = "Scissors cut Paper! \n";
+const playerWin = "YOU WIN! \n";
+const playerLose = "YOU LOSE! \n";
+const playerTie = "It's a tie! \n";
 
+let playerWinCount = 0;
+let computerWinCount = 0;
+
+//Player buttons & event listeners
+const rockButton = document.querySelector('#rockbutton').addEventListener('click', (rockEvent) => {
+    gameRound(rockEvent);
+});
+const paperButton = document.querySelector('#paperbutton').addEventListener('click', (paperEvent) => {
+    gameRound(paperEvent);
+});
+const scissorsButton = document.querySelector('#scissorsbutton').addEventListener('click', (scissorsEvent) => {
+    gameRound(scissorsEvent);
+});
+
+//Function to randomly generate computer selection
 function getComputerChoice() {
     const random = Math.floor(Math.random() * choices.length);
     return choices[random];
 };
 
-function gameRound() {
-    let computerSelection = getComputerChoice();
-    let playerSelection = prompt("Rock, paper, or scissors?").toUpperCase();
+//Function that runs computer selection vs player selection, and displays who wins
+function gameRound(event) {
 
-    document.write('You chose: ' + playerSelection + ". <br>");
-    document.write('Your opponent chose: ' + computerSelection + ". <br>");
+    let playerSelection = event.target.innerText;
+    let computerSelection = getComputerChoice();
+
+    let div = document.querySelector('#text');
+    div.innerText = 'You chose: ' + playerSelection + ".\n" + 'Your opponent chose: ' + computerSelection + ".";
+
+    let scoreCounter = document.querySelector('#scoreCounter');
+    let matchWinner = document.querySelector('#matchWinner');
 
     let rock_vScissors = (playerSelection === "ROCK" && computerSelection === "SCISSORS");
     let rock_vPaper = (playerSelection === "ROCK" && computerSelection === "PAPER");
@@ -28,30 +50,41 @@ function gameRound() {
     let gameTie = (playerSelection === computerSelection);
     let gameError = (!choices.includes(playerSelection));
 
-        if (rock_vScissors) {
-            document.write(scissorsRock, playerWin);
-        } else if (rock_vPaper) {
-            document.write(rockPaper, playerLose);
-        } else if (scissors_vPaper) {
-            document.write(paperScissors, playerWin);
-        } else if (scissors_vRock) {
-            document.write(scissorsRock, playerLose);
-        } else if (paper_vRock) {
-            document.write(rockPaper, playerWin);
-        } else if (paper_vScissors) {
-            document.write(paperScissors, playerLose);
-        } else if (gameTie) {
-            document.write(playerTie);
-        } else if (gameError) {
-            document.write("Error: Invalid input. Please refresh and try again.<br>")
-        }  
-}; 
+    if (rock_vScissors) {
+        matchWinner.innerText = scissorsRock + playerWin;
+        playerWinCount +=1;
+    } else if (rock_vPaper) {
+        matchWinner.innerText = rockPaper + playerLose;
+        computerWinCount +=1;
+    } else if (scissors_vPaper) {
+        matchWinner.innerText = paperScissors + playerWin;
+        playerWinCount +=1;
+    } else if (scissors_vRock) {
+        matchWinner.innerText = scissorsRock + playerLose;
+        computerWinCount +=1;
+    } else if (paper_vRock) {
+        matchWinner.innerText = rockPaper + playerWin;
+        playerWinCount +=1;
+    } else if (paper_vScissors) {
+        matchWinner.innerText = paperScissors + playerLose;
+        computerWinCount +=1;
+    } else if (gameTie) {
+        matchWinner.innerText = playerTie;
+    } else if (gameError) {
+        matchWinner.innerText = "Error: Invalid input. Please refresh and try again.<br>"
+    };  
+    scoreCounter.innerText = "Score counter: " + "player: " + playerWinCount + ", computer: " + computerWinCount;
 
-function game() {
-    for (let gameCounter = 1; gameCounter < 6; gameCounter++) {
-        console.log("Game counter: " + gameCounter);
-        setTimeout(gameRound, 2*1000);
-    }
+    //To see if 1) function is running and 2) counter is working
+    console.log("Score counter 2: " + "player: " + playerWinCount + ", computer: " + computerWinCount);
+
+    if (playerWinCount === 5) {
+        document.write("YOU WON!");
+        document.write("<br>Player score: " + playerWinCount);
+        document.write("<br>Computer score: " + computerWinCount);
+    } else if (computerWinCount === 5) {
+        document.write("YOU LOST!");
+        document.write("<br>Computer score: " + computerWinCount);
+        document.write("<br>Player score: " + playerWinCount);
+    };
 };
-
-game();
